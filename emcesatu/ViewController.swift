15840661af6cde4725e5babfa025c09a09ec0c3d
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-04-07 17:14:08 +0700

    Initial Commit

---
+++ emcesatu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  emcesatu
+//
+//  Created by Marshall Kurniawan on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
